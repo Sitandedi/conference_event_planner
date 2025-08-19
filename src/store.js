67466ac37dc2b4454a4cbca06220f96913a7ed92
Sitandedi <f.nyamsi@gmail.com>
2025-08-18 20:01:43 -0400

    4rd commit

--- src/store.js
+++ src/store.js
@@ -2,10 +2,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
 import avReducer from './avSlice';
+import mealsReducer from './mealsSlice';
 
 export default configureStore({
   reducer: {
     venue: venueReducer,   // state.venue is managed by venueReducer. all components in the application can access the state managed by the venueReducer()
     av: avReducer,
+    meals: mealsReducer,
   },
 });
